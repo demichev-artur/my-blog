ef6fe860b26161d08d70af44367bd3ecf405eb31
demichev-artur <artur.demichev03@mail.ru>
2022-07-21 18:07:53 +0600

    refactor: added map func in components

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -10,23 +10,22 @@ const Tags = () => {
 
     const tagsArray = tags.map(
         item => (
-        <span
-            className="w3-tag w3-light-grey w3-small w3-margin-bottom"
-            style={margin}>
+            <span
+                className="w3-tag w3-light-grey w3-small w3-margin-bottom"
+                style={margin}>
             {item}
         </span>)
     )
 
     return (
         <>
-            {/* Labels / index */}
             <div className="w3-card w3-margin">
                 <div className="w3-container w3-padding">
                     <h4>Tags</h4>
                 </div>
                 <div className="w3-container w3-white">
                     <p>
-                        { tagsArray }
+                        {tagsArray}
                     </p>
                 </div>
             </div>
--- src/contants/index.js
+++ src/contants/index.js
@@ -4,4 +4,78 @@ export const tags = [
     'Travel', 'New York', 'London', 'IKEA', 'NORWAY', 'DIY', 'Ideas', 'Baby', 'FamilyNews', 'Clothing', 'ShoppingSports', 'Games'
 ]
 
+export const posts = [
+    {
+        src: "https://www.w3schools.com/w3images/workshop.jpg",
+        alt: "Image",
+        classNameImg: "w3-left w3-margin-right",
+        style: {width: 50},
+        title: 'Lorem',
+        classNameTitle: 'w3-large',
+        text: 'Sed mattis nunc'
 
+    },
+    {
+        src: "https://www.w3schools.com/w3images/gondol.jpg",
+        alt: "Image",
+        classNameImg: "w3-left w3-margin-right",
+        style: {width: 50},
+        title: 'Ipsum',
+        classNameTitle: 'w3-large',
+        text: 'Praes tinci sed'
+
+    },
+    {
+        src: "https://www.w3schools.com/w3images/skies.jpg",
+        alt: "Image",
+        classNameImg: "w3-left w3-margin-right",
+        style: {width: 50},
+        title: 'Dorum',
+        classNameTitle: 'w3-large',
+        text: 'Ultricies congue'
+
+    },
+    {
+        src: "https://www.w3schools.com/w3images/rock.jpg",
+        alt: "Image",
+        classNameImg: "w3-left w3-margin-right",
+        style: {width: 50},
+        title: 'Mingsum',
+        classNameTitle: 'w3-large',
+        text: 'Lorem ipsum dipsum'
+
+    },
+]
+
+export const blockInfo = [
+    {
+        blockImgSrc: 'https://www.w3schools.com/w3images/woods.jpg',
+        blockImgAlt: 'Nature',
+        blockImgStyle: {width: "100%"},
+        title: 'TITLE HEADING',
+        date: 'April 7, 2014',
+        blockText: 'Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed\n' +
+            '                        mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac\n' +
+            '                        accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam\n' +
+            '                        corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo\n' +
+            '                        condimentum, porta lectus vitae, ultricies congue gravida diam non\n' +
+            '                        fringilla.',
+        countComments: '0',
+        classCountComments: 'w3-tag'
+    },
+    {
+        blockImgSrc: 'https://www.w3schools.com/w3images/bridge.jpg',
+        blockImgAlt: 'Norway',
+        blockImgStyle: {width: "100%"},
+        title: 'BLOG ENTRY',
+        date: 'April 2, 2014',
+        blockText: 'Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed\n' +
+            '                        mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac\n' +
+            '                        accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam\n' +
+            '                        corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo\n' +
+            '                        condimentum, porta lectus vitae, ultricies congue gravida diam non\n' +
+            '                        fringilla.',
+        countComments: '2',
+        classCountComments: 'w3-badge'
+    }
+]
