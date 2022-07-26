fb7a109805ea176f3bbbeea9d625745e890f046f
demichev-artur <artur.demichev03@mail.ru>
2022-07-26 08:23:17 +0600

    refactor: selection of tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -9,11 +9,27 @@ const Tag = props => {
 
     const [selectTag, setSelectTag] = useState(false)
 
+
+    const check = () => {
+        const elements = document.querySelectorAll('#tagsInto span');
+
+        setSelectTag(!selectTag);
+
+        for (let i = 0; i < elements.length; i++) {
+
+            if(elements[i].classList.contains('w3-black')){
+                setSelectTag(false);
+            }
+
+        }
+
+    }
+
     return (
 
         <span
-            onClick={() => setSelectTag(!selectTag)}
-            className={selectTag ? "w3-tag w3-black w3-margin-bottom" : "w3-tag w3-light-grey w3-small w3-margin-bottom"}
+            onClick={() => check()}
+            className={ selectTag ? "w3-tag w3-black w3-margin-bottom" : "w3-tag w3-light-grey w3-small w3-margin-bottom" }
             style={margin}>
             {props.name}
         </span>
@@ -24,7 +40,7 @@ const Tag = props => {
 
 const Tags = () => {
 
-    const tagsArray = tags.map((item, i) => <Tag name={item} tabindex={i}/>)
+    const tagsArray = tags.map(item => <Tag name={item}/>)
 
     return (
         <>
@@ -33,7 +49,7 @@ const Tags = () => {
                     <h4>Tags</h4>
                 </div>
                 <div className="w3-container w3-white">
-                    <p>
+                    <p id="tagsInto">
                         {tagsArray}
                     </p>
                 </div>
